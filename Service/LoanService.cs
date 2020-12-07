09c09dab92388ec43b3da33e5e0d5606d7532a15
bencart65 <72330125+bencart65@users.noreply.github.com>
2020-12-07 14:36:18 -0600

    Backend push

--- Service/LoanService.cs
+++ Service/LoanService.cs
@@ -5,12 +5,23 @@ using System.Collections.Generic;
 class LoanService
 {
 
-    public List<Cashflow> void CalculateCashflows(List<LoanItems> TagLoans)
+    public List<Cashflow> CalculateCashflows(List<LoanItems> TagLoans)
     {
         List<Cashflow> CashflowList = new List<Cashflow>();
-
+        Cashflow aggregateCashflow = new Cashflow();
+        int longestLoan = 0;
         foreach( LoanItem loan in TagLoans){
-            
+            if(loan.Term > longestLoan){
+                longestLoan = loan.Term;
+            }
+        }
+        int i = longestLoan;
+        while(i != 0){
+            foreach( LoanItem loan in TagLoans){
+                var loanRate = loan.Rate;
+                
+            }
+            i -= 1;
         }
         if(TagLoans.Count == 1){
             Cashflow cashflow = new Cashflow()
