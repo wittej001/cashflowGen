ce027555b44a6f3d23b6215a05d51ca8d100ab67
Matthew Smith-Erb <mcse@maine.rr.com>
2020-12-07 15:48:32 -0500

    Added table-view functionality

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoanDetailComponent } from './loan-detail/loan-detail.component';
+import { TableViewComponent } from './table-view/table-view.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoanDetailComponent,
+    TableViewComponent
   ],
   imports: [
     BrowserModule,
---
+++ frontend/src/app/table-view/table-view.component.ts
@@ -0,0 +1,82 @@
+import { Component, OnInit } from '@angular/core';
+import { Loan } from '../Loan';
+import { LoanService } from '../loan.service'
+
+@Component({
+  selector: 'app-table-view',
+  templateUrl: './table-view.component.html',
+  styleUrls: ['./table-view.component.css']
+})
+export class TableViewComponent implements OnInit {
+  //Ask Dale why we need to initialize all of these but didn't have to in the tutorial
+  AllData : Loan[] = [];
+  loans : any[] = [{
+    type: "loan",
+    principal: 16000,
+    term: 16,
+    rate: 6,
+    cashflow: [
+           {
+             month: 1,
+             principal: 100,
+             interest:  1,
+             remaining_balance: 1
+            },
+           {
+             month: 2,
+             principal: 2,
+             interest:  2,
+             remaining_balance: 2
+            },
+            {
+             month: 3,
+             principal: 3,
+             interest:  3,
+             remaining_balance: 3
+            }
+     ]
+   },
+   {
+    type: "loan",
+    principal: 16000,
+    term: 16,
+    rate: 6,
+    cashflow: [
+           {
+             month: 1,
+             principal: 100,
+             interest:  1,
+             remaining_balance: 1
+            },
+           {
+             month: 2,
+             principal: 2,
+             interest:  2,
+             remaining_balance: 2
+            },
+            {
+             month: 3,
+             principal: 3,
+             interest:  3,
+             remaining_balance: 3
+            }
+     ]
+   },];
+  aggregate : any;
+
+  constructor(/**private loanSerivce : LoanService**/) { }
+
+  ngOnInit(): void {
+    //this.getAllData();
+
+  }
+
+  newLoan(balance: string, term: string, interest: string){
+    alert("added loan");
+  }
+  /**getAllData() : void{
+    this.loanSerivce.getAllData()
+    .subscribe(AllData => this.AllData = AllData);
+  }**/
+}
+
